1c15b089b918fda67b0c4afbd7c98136f4a22cf3
BojOnTheBeat <bjfatade@gmail.com>
2020-05-22 20:38:41 -0400

    flesh out tracer and add unit tests

---
+++ tracer.go
@@ -0,0 +1,26 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code
+type Tracer interface {
+	Trace(...interface{}) //...interface means Trace accepts 0 or more args of any type
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
+// New does stuff
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
