4fa1cfda696598b15f3d2a7549505740c3652751
BojOnTheBeat <bjfatade@gmail.com>
2020-05-25 17:09:45 -0400

    comment

--- tracer_test.go
+++ tracer_test.go
@@ -19,6 +19,9 @@ func TestNew(t *testing.T) {
 }
 
 func TestOff(t *testing.T) {
+
+	// It's probably better to omit Tracer type and let type inferrence work here
+	// but I'll leave this for education purposes
 	var silentTracer Tracer = Off()
 	silentTracer.Trace("something")
 }
